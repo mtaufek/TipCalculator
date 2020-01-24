a83ef51d0b006f845e10e862e09cd37bf086a037
Adib Thaqif <adibthaqifx@gmail.com>
2020-01-23 17:07:37 -0800

    Initial Commit

---
+++ Tippy2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tippy2
+//
+//  Created by Adib Thaqif on 1/23/20.
+//  Copyright © 2020 Adib Thaqif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
